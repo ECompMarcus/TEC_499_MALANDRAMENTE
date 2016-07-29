3d4ba3aadc6ecdea79f6c79dedb87b33a0ef79f4
Marcus José <marcusjose@live.com>
2016-07-29 14:40:46 -0300

    Erro
    Correção

--- ProgramasTestes/potencia.asm
+++ ProgramasTestes/potencia.asm
@@ -1,18 +1,14 @@
-#	while(expo>1){
-#		base = base*base;
-#		expo--;
-#	}
-
-
+.module potencia
+.pseg
 main:	
 	li $t0,1
-	li $s2, 3
-	move $s0, $s2 # base
-	beq $s0, 0, Exit
-	addi $s1, $zero, 3 # expoente
+	li $s2, 3; defina base aqui
+	move $s0, $s2; base
+	beq $s0, $zero, exit; caso a base seja 0
+	addi $s1, $zero, 3; defina o expoente aqui (troque o 3 pelo valor desejado)
 loop:
-	mul $s0, $s0, $s2
-	addi $s1, $s1, -1
-	bne  $s1, $t0, loop
-Exit:
+	mul $s0, $s0, $s2; multiplica base*base
+	addi $s1, $s1, -1; subtrai expoente
+	bne  $s1, $t0, loop; enquanto não for igual
+exit:
 	syscall			# Exit!
