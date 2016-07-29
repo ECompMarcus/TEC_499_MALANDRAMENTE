f11460db7d2309b69f287370d8c730ac40381c31
Marcus José <marcusjose@live.com>
2016-07-29 14:26:57 -0300

    fibonancci
    modificado estrutura

--- ProgramasTestes/fibonacci.asm
+++ ProgramasTestes/fibonacci.asm
@@ -1,37 +1,37 @@
-main: addi $sp, $sp, -4
-sw $ra, 0($sp)
+.module fibonacci
+.pseg
+main: 
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	addi $a0, $zero, 3
+	jal fibo
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 
-addi $a0, $zero, 3
-jal fibo
+fibo: 
+	addi $sp, $sp, -12
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	sw $ra, 8($sp)
+	slti $t0, $a0, 2; se $a0 menor, $t0=1
+	beq $t0, $zero, else; se igual
+	addi $v0, $zero, 1
+	jr $ra
 
-lw $ra, 0($sp)
-addi $sp, $sp, 4
+else: 
+	addi $s0, $a0, 0
+	addi $a0, $a0, -1
+	jal fibo
+	addi $s1, $v0, 0
+	addi $a0, $s0, -2
+	jal fibo
+	add $s1, $s1, $v0
+	j EXIT
 
-fibo: addi $sp, $sp, -12
-sw $s0, 0($sp)
-sw $s1, 4($sp)
-sw $ra, 8($sp)
-
-slti $t0, $a0, 2
-beq $t0, $zero, ELSE
-
-addi $v0, $zero, 1
-jr $ra
-
-ELSE: addi $s0, $a0, 0
-addi $a0, $a0, -1
-jal fibo
-
-addi $s1, $v0, 0
-addi $a0, $s0, -2
-jal fibo
-
-add $s1, $s1, $v0
-j EXIT
-
-EXIT: lw $s0, 0($sp)
-lw $s1, 4($sp)
-lw $ra, 8($sp)
-addi $sp, $sp, 12
-
-jr $ra
+exit: 
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	jr $ra
+.end
