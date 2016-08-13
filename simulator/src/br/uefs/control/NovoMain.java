421b69ff48f3afb31420fd375f75a659793b5fd7
Marcus Jose <MarcusJoseEComp@users.noreply.github.com>
2016-08-13 11:41:12 -0300

    Update NovoMain.java

--- simulator/src/br/uefs/control/NovoMain.java
+++ simulator/src/br/uefs/control/NovoMain.java
@@ -22,23 +22,6 @@ public class NovoMain {
      */
     public static void main(String[] args) {
         try {
-            // TODO code application logic here
-            /*String valor = "00000010001100101000000000100000";
-            String opcode = "0b" + valor.substring(0, 6);
-            String op1 = "0b" + valor.substring(6, 11);
-            String op2 = "0b" + valor.substring(11, 16);
-            String op3 = "0b" + valor.substring(16, 21);
-            String deslocamento = "0b" + valor.substring(21, 26);
-            String function = "0b" + valor.substring(26, 32);
-            String bit_16_31 = "0b" + valor.substring(16, 32);
-            String bit_11_31 = "0b" + valor.substring(11, 32);
-            String bit_6_31 = "0b" + valor.substring(6, 32);
-            
-            System.out.println(bit_16_31);
-            int i = Integer.parseInt(bit_16_31);
-            System.out.println(String.valueOf(i));
-            */
-            
             UnitControl uc = new UnitControl();
             
         } catch (IOException ex) {
