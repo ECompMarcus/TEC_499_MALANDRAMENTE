7a3cbfc186f3ea3dff1c1453abc6bfee1031abb0
Marcus Jose <MarcusJoseEComp@users.noreply.github.com>
2016-08-29 00:18:59 -0300

    Update Reg_IF_ID.v

--- PBL_2/Meta_2/Reg_IF_ID.v
+++ PBL_2/Meta_2/Reg_IF_ID.v
@@ -1,16 +1,16 @@
 // Universidade Estadual de Feira de Santana 
 // TEC499 - MI - Sistemas Digitais
 //
-// Module: Reg_ID_IF
+// Module: Reg_IF_ID
 
-module  Reg_ID_IF( Next_Instruct_IN, Current_instruct_IN, 
+module  Reg_IF_ID( Next_Instruct_IN, Current_instruct_IN, 
 						 Next_Instruct_OUT, Current_instruct_OUT, 
-						 clock, clear, Read_Write);
-	input [31:0] Next_Instruct_IN, Current_instruct_IN;
-	output [31:0] Next_Instruct_OUT, Current_instruct_OUT;
-	reg [31:0] temp1, temp2;
-	reg  [31:0] Next_Instruct_OUT, Current_instruct_OUT;
-	input clock, clear, Read_Write;
+						 clock, clear, Write);
+	input 	[31:0] Next_Instruct_IN, Current_instruct_IN;
+	output 	[31:0] Next_Instruct_OUT, Current_instruct_OUT;
+	
+	reg  		[31:0] Next_Instruct_OUT, Current_instruct_OUT;
+	input clock, clear, Write;
 
 	initial
 		begin
@@ -21,21 +21,14 @@ module  Reg_ID_IF( Next_Instruct_IN, Current_instruct_IN,
 	always @ (posedge clock)
 		begin
 		if (clear)
-			begin
-				temp1 = 32'b0;
-				temp2 = 32'b0; 
+			begin				
 				Next_Instruct_OUT = 32'b0;
 				Current_instruct_OUT = 32'b0;
 			end 
-		if (Read_Write ==0 && clear == 0)
+		if (Write ==1 && clear == 0)
 			begin
-				Next_Instruct_OUT = temp1;
-				Current_instruct_OUT = temp2;
-			end
-		if (Read_Write ==1 && clear == 0)
-			begin
-				temp1 = Next_Instruct_IN;
-				temp2 = Current_instruct_IN;
+				Next_Instruct_OUT = Next_Instruct_IN;
+				Current_instruct_OUT = Current_instruct_IN;
 			end				
 		end
 endmodule
